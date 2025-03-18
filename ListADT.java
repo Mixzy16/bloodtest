ddc8dc03237e5871f1da6a11de3902cdb6dde3c4
Mixzy16 <116266238+Mixzy16@users.noreply.github.com>
2025-03-18 13:36:02 +0000

    added the list adt

---
+++ ListADT.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bloodtest;
+
+/**
+ *
+ * @author micha
+ */
+public interface ListADT<T> {
+    void add(T item);
+    T get(int index);
+    int size();
+}
